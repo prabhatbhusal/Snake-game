6ba0a6682666f431eeb2e5ad78ac8b1cdd5383ca
Prabhat Bhusal <prabhatbhusal777@gmail.com>
2024-11-11 21:28:53 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
-from turtle import Turtle,Screen
-import random
-tim=Turtle()
-ram=Turtle()
-sham=Turtle()
-timmy=Turtle()
-screen=Screen()
-screen.setup(width=500,height=400)
-user_bet=screen.textinput(title="Make your bet",prompt="Which turtle will win the race? Enter a color: ")
-print(user_bet)
-tim.color('red')
-ram.color('yellow')
-sham.color('green')
-timmy.color('blue')
-tim.shape('turtle')
-sham.shape('turtle')
-ram.shape('turtle')
-timmy.shape('turtle')
-tim.penup()
-timmy.penup()
-sham.penup()
-ram.penup()
-tim.goto(-230,80)
-timmy.goto(-230,40)
-ram.goto(-230,00)
-sham.goto(-230,-40)
-tim.penup()
-print("Hello World")
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
 
-print("You lose, ")
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
